3618ad3d8334c131914ed8e29ff0bd0cb96682e3
Andre <als260602@msn.com>
2021-02-23 16:38:46 -0300

    iniciando projet nextjs

---
+++ src/components/CompletedChallengers.tsx
@@ -0,0 +1,11 @@
+import styles from '../styles/components/CompletedChallengers.module.css';
+
+export function CompletedChallengers() {
+
+  return (
+    <div className={styles.completedChallengersContainer}>
+      <span>Desafios completos</span>
+      <span>06</span>
+    </div>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,28 @@
+import Head from 'next/head';
+import { CompletedChallengers } from '../components/CompletedChallengers';
+import { Countdown } from '../components/Countdown';
+import { ExperienceBar } from "../components/ExperienceBar";
+import { Profile } from '../components/Profile';
+
+import styles from '../styles/pages/Home.module.css';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <ExperienceBar />
+
+      <section>
+        <div>
+          <Profile />
+          <CompletedChallengers />
+          <Countdown />
+        </div>
+
+        <div>
+
+        </div>
+      </section>
+    </div>
+
+  )
+}

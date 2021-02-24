a3b00fed2956411d001abb59303baceb3ffd9982
Andre <als260602@msn.com>
2021-02-24 15:35:20 -0300

    ADD: context API para convrsar entre componentes

--- src/components/CompletedChallengers.tsx
+++ src/components/CompletedChallengers.tsx
@@ -1,11 +1,13 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../context/ChallengesContext';
 import styles from '../styles/components/CompletedChallengers.module.css';
 
 export function CompletedChallengers() {
-
+  const { challengesCompleted } = useContext(ChallengesContext);
   return (
     <div className={styles.completedChallengersContainer}>
       <span>Desafios completos</span>
-      <span>06</span>
+      <span>{challengesCompleted}</span>
     </div>
   );
 }
